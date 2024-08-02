cff1c73e27678cd638d3e77315b6f088677fb629
forcaweb <forcawebsuporte@gmail.com>
2024-08-01 22:42:55 -0300

    at0108202422

--- demon-dao-jdbc/src/application/Program.java
+++ demon-dao-jdbc/src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
+import exceptions.DBException;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -23,6 +25,34 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("========= Test 3: FindAll ==========");
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("========= Test 4: Insert ==========");
+		Seller newSeller = new Seller(null, "Thais", "thaislima@teste.com", new Date(), 3000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Seller id adicionado = " + newSeller.getId());
+		
+		System.out.println("========= Test 5: Update ==========");
+		seller = sellerDao.findId(2);
+		seller.setName("Britão");
+		sellerDao.update(seller);
+		System.out.println("Update concluido!");
+		
+		System.out.println("========= Test 6: Delete ==========");
+		seller = sellerDao.findId(12);
+		if(seller != null) {
+			sellerDao.deleteById(seller.getId());
+		}else {
+			throw new DBException("Seller não existe!");
+		}
+		
+		System.out.println("Delete concluido!");
 	}
 
 }
